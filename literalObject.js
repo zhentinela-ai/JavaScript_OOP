eeaf9f4b5605d0dd6b1a54101b4396e10ec72e79
zhentinela-ai <>
2022-07-15 04:44:37 +0000

    literal object

---
+++ literalObject.js
@@ -0,0 +1,18 @@
+console.log( { } );
+
+let name = "Ryan";
+let lastname = "Ray";
+let age = 30;
+
+const user = {
+  name: "Rayn",
+  lastname: "Ray",
+  age: 30,
+  hobbies: ["read", "programming", "run"],
+  address: {
+    street: "some street",
+    city: "London"
+  }
+}
+
+console.log(user)

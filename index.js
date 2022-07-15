853f2506b29db6f7e3996421e02693f07c5103cd
zhentinela-ai <>
2022-07-15 04:38:27 +0000

    Almost Everything is an Object

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log(typeof "Hello World");
+console.log(typeof 100);
+console.log(typeof false);
+
+console.log(typeof { });
+console.log(typeof [ ]);
+
+console.log(typeof new Date());
+console.log(typeof new RegExp());
+
+console.log( new Number(100));
+console.log( new String("Hello World"));
+console.log( new Boolean(true));
+

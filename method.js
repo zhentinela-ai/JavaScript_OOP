669e2dfd9e6321cf14e97f8aa86f635794ef8978
zhentinela-ai <>
2022-07-15 04:56:42 +0000

    methods

---
+++ method.js
@@ -0,0 +1,13 @@
+
+
+const user = {
+  name: "Joe", //properties
+  lastName: "McMillan",
+  age: 30,
+  // showFullName: function() {
+  showFullName() {
+    return "Ryan Ray"
+  }
+}
+
+console.log(user.showFullName())

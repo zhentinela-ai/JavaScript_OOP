2a9779d92e4d911e3cd228c8512a70c5fe47eef6
zhentinela-ai <>
2022-07-15 06:46:04 +0000

    OOP principles

---
+++ src/oopPrinciples.js
@@ -0,0 +1,28 @@
+`Principles?
+Object Oriented Languages promote: modularity & code reusability
+There is not a specification or technical document for OOP
+It's based from common sense from papers of aerly reseachers`
+
+`Two accepted definitins: for define a programming as OO`
+`Capability to model problems through objects`
+`Support few principles to grant modularity and code reuse`
+
+`1st requirement:
+Model through Objects: describe reality using object and its relationships`
+`- Association
+    Object capability to refer another object`
+`- Aggregation
+    object capability to refer one or more independent objects`
+`- Composition
+    object capability to refer one or more dependent objects`
+
+`2nd requirement
+OO principles: grant modularity & code reuse`
+`- Encapsulation
+    capability to concentrate into a single entity data and code that manipulates it. hidden internal datails`
+`- Inheritance
+    mechanism by which object acquires some or all features of one or more objects`
+`- Polymorphism
+    capability to process objects differently based on their data type or structure`
+
+`OOP Language: typically these requirements classify a language as OO`
